1dce4f52c8e446c66bd86a8ab1a1e9aa11cd6838
Ubun21 <heweijie.2013@gmail.com>
2023-03-18 17:52:37 +0800

    for test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   overrides: [],
-  parser: 'vue-eslint-parser',
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
   },
-  ignorePatterns: ['**/dist'],
-  plugins: ['vue', '@typescript-eslint'],
+  ignorePatterns: ["**/dist"],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/no-multiple-template-root': 'off'
-  }
-}
+    "vue/multi-word-component-names": "off",
+    "vue/no-multiple-template-root": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+  },
+};
